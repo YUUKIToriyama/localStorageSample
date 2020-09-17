ea95d48539682f3d2f2c4b46b44e4accb1ed7b51
YUUKIToriyama <ytoriyama@torisfactory.com>
2020-09-17 19:48:08 +0900

    オートフィル機能を追加

--- main.js
+++ main.js
@@ -2,20 +2,27 @@
 
 window.onload = () => {
 	const loginForm = document.getElementById("loginForm");
+	//autoFill();
 	loginForm["loginButton"].addEventListener("click", () => {
-		let userName = loginForm["userName"].value;
-		let password = loginForm["password"].value;
-
-		if (loginForm["saveProfile"].checked === true) {
-			localStorage.setItem("userName", userName);
-			localStorage.setItem("password", password);
-		}
+		saveProfile();
 		alert("now loading...");
 	})
+}
+
+const saveProfile = () => {
+	let userName = loginForm["userName"].value;
+	let password = loginForm["password"].value;
+
+	if (loginForm["saveProfile"].checked === true) {
+		localStorage.setItem("userName", userName);
+		localStorage.setItem("password", password);
+	}
 }
 
 const autoFill = () => {
 	if (localStorage.getItem("userName") != undefined) {
 		// localStorageにuserNameの情報があるときオートフィルを行なう
+		loginForm["userName"].value = localStorage.getItem("userName");
+		loginForm["password"].value = localStorage.getItem("password");
 	}
 }
